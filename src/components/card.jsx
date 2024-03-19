f33dcc53fd6d6af2dadb8d2f0f83ef5fb9109bf1
shahid1170 <96632432+shahid1170@users.noreply.github.com>
2024-03-19 22:38:08 +0500

    Update card.jsx

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -7,7 +7,7 @@ function ImgCard(){
       {product.map(product => (
         <div className="col" key={product.id}>
           <div className="card" >
-            <img src={product.image} className="card-img-top logo" alt="..." />
+            <img src={product.image} className="card-img-top" alt="..." />
             <div className="card-body">
               <h5 className="card-title">{product.title}
                 {" "}<span className="badge text-bg-primary">Rs {product.price}</span></h5>
